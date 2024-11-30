7a17d6095dcee48d7e08f350c2a7159fb2c5426f
rodjenihm <rodjenihm@gmail.com>
2024-11-30 13:54:26 +0100

    Configure Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -79,6 +80,7 @@ export default function RootLayout({
             }}
           />
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
